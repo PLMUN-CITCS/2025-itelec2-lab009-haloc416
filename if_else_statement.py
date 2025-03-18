ea69382334d6fb4458d426eb6b44be2ba79ed4e9
Marco Haloc <halocmarco_bsit@plmun.edu.ph>
2025-03-18 21:04:08 +0800

    Submitting Python Week 04 - Laboratory # 09

---
+++ if_else_statement.py
@@ -0,0 +1,12 @@
+user_input = input("Enter a number: ")
+
+try:
+    number = int(user_input)
+    
+    if number % 2 == 0:
+        print("The number", number, "is Even.")
+    else:
+        print("The number", number, "is Odd.")
+
+except ValueError:
+    print("Invalid input. Please enter an integer.")
